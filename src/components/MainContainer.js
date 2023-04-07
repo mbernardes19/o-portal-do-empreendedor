d74b0e357d612e6949f4cef15d1b323f93d4b5da
Mr-Kobayash <danielvh2lbk@gmail.com>
2023-04-06 21:26:07 -0300

    emplemantacao do rodape

---
+++ src/components/MainContainer.js
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+import styles from '../styles/MainContainer.module.css'
+
+export default function MainContainer ({children}) {
+    return (
+        <>
+        <div className={styles.container}>
+            {children}
+        </div>
+        <Footer />
+        </>
+    )
+}
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,66 @@
-import footerData from '../../content/footer/footer.json'
+import styles from '../styles/Footer.module.css'
 
-export default function Footer() {
-    const {copyrightText, sections} = footerData
+export default function Footer () {
     return (
-        <footer>
-            <div>
-                {sections && sections.map(section => (
-                    <div key={section.title}>
-                        <h1>{section.title}</h1>
-                        <ul>
-                            {section.links.map(link => (
-                                <li key={link.name}><a href={link.url}>{link.name}</a></li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
+        <footer /*className={styles.footer}*/>
+
+
+
+            <div className={styles.container}>
+
+            <div className={styles.filho}>
+            <h1  className={styles.titulo_um}>Fale Conosco</h1>
+
+            <h3 className={styles.subtitulo_um} >HORÁRIO DE ATENDIMENTO: </h3>
+
+            <p className={styles.parag_atendimento} >Seg à Sex / 8:00 - 22:00 <br></br>
+               Sab / 9:30 - 17:00</p>
+            
+            <h3 className={styles.subtitulo_dois} >CONTATO: </h3>
+            <a href='#' className={styles.link_email}>contato@umcontatoaleatorio.com</a>
+
+            <h3 className={styles.subtitulo_tres} >ENDEREÇO: </h3>
+            <p className={styles.parag_endereco}>RUA ALEATORIA, 000, SALA 0000-C<br></br>
+               Setor ALEATORIO, BRASIL - BR
+            </p>
             </div>
-            <p>{copyrightText}</p>
+
+            <div className={styles.neto} >
+            <h1  className={styles.titulo_dois}>Serviços</h1>
+             
+             <ul className={styles.ul} >
+
+                <li  className={styles.list_servicos}><a  href='#' className={styles.link_servicos}>Registro de CNPJ MEI</a></li>
+                <li  className={styles.list_servicos}><a className={styles.link_servicos} href='#'>Alterações Cadastrais do MEI</a></li>
+                <li  className={styles.list_servicos}><a className={styles.link_servicos} href='#'>Baixar/Cancelar CNPJ MEI</a></li>
+                <li  className={styles.list_servicos}><a className={styles.link_servicos} href='#'>Declaração Anual DASN-SMEI</a></li>
+             </ul>
+            </div>
+
+            <div className={styles.netinho}>   
+                <h1 className={styles.titulo_tres}>Sobre o Site</h1>
+           <ul className={styles.ul} >
+
+            <li className={styles.list_sobresite}>
+                <a href='#' target='__blank'  className={styles.link_sobresite}>Aviso de Privacidade</a>
+            </li>
+
+            <li className={styles.list_sobresite}>
+                <a  href='#' target='__blank'  className={styles.link_sobresite}>Termos de Uso</a>
+            </li>
+            
+            <li className={styles.list_sobresite}>
+                <a  href='#' target='__blank' className={styles.link_sobresite}>Quem Somos</a>
+            </li>
+
+           </ul>
+            </div>
+           
+
+            
+            </div>
+             <hr className={styles.linha}></hr>
+            <p className={styles.direitos}>&copy; Copyright 2023 -CNPJ:00.000.000/0001-00 - EMPRESA . Todos os Direitos Reservados</p>
         </footer>
     )
 }
