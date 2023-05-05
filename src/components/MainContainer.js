58a6bf5193eaec96e351b9efd1c9e225919546ea
Matheus Bernardes <mbernardes@corra.com>
2023-05-05 10:47:33 -0300

    fixes issues

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,7 +2,7 @@ import Formu from "./Cabecalho";
 import Info from "./Info";
 import Section from "./Section";
 import Faq from "./Faq";
-import Footer from "./Footer";
+import Footer from "./footer";
 
 export default function MainContainer({ children }) {
   return (
